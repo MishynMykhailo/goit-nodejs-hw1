28cb6eef6f62a7a75ff90a3a404627f21c3d56a5
MishynMykhailo <misham18092001@gmail.com>
2022-08-06 16:18:47 +0300

    add readme and done HW

---
+++ README.md
@@ -0,0 +1,17 @@
+# Получаем и выводим весь список контактов в виде таблицы (console.table)
+
+`node index.js --action list`
+
+# Получаем контакт по id
+
+`node index.js --action get --id 5`
+
+# Добавялем контакт
+
+`node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22`
+
+# Удаляем контакт
+
+`node index.js --action remove --id=3`
+
+<a href="https://ibb.co/mSr6qLq"><img src="https://i.ibb.co/pn6Jztz/Screenshot-2.png" alt="Screenshot-2" border="0"></a>
--- contacts.js
+++ contacts.js
@@ -1,61 +1,75 @@
-// const fs = require("fs/promises");
-// const { get } = require("http");
-// const { type } = require("os");
-// const path = require("path");
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
 
-// const contactsPath = path.resolve("./db/contacts.json");
+const contactsPath = path.resolve("./db/contacts.json");
 
-// console.log(contactsPath);
-// // TODO: задокументировать каждую функцию
-// async function listContacts() {
-//   const data = await fs.readFile(contactsPath, "utf8");
-//   try {
-//     const normalizeData = data;
+const updateContacts = async (contact) => {
+  await fs.writeFile(contactsPath, JSON.stringify(contact, null, 2));
+};
 
-//     // result code!!!
+// fn get all contacts
+async function listContacts() {
+  const data = await fs.readFile(contactsPath, "utf8");
+  try {
+    const normalizeData = JSON.parse(data);
+    return normalizeData;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
 
-//     // console.log(JSON.parse(normalizeData));
+//fn get contact by ID
+async function getContactById(contactId) {
+  const contacts = await fs.readFile(contactsPath);
+  try {
+    const normalizeData = JSON.parse(contacts);
+    const result = normalizeData.find(
+      ({ id }) => parseInt(id) === parseInt(contactId)
+    );
+    console.log(typeof contactId, contactId);
+    return result;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
 
-//     return normalizeData;
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// }
-// listContacts();
-// async function getContactById(contactId) {
-//   const contacts = await fs.readFile(contactsPath);
-//   try {
-//     const normalizeData = JSON.parse(contacts);
-//     const result = normalizeData.filter(({ id }) => parseInt(id) === contactId);
+// fn delete Contact by ID
+async function removeContact(contactId) {
+  try {
+    const contacts = await listContacts();
+    const idx = contacts.findIndex((item) => item.id === contactId);
+    if (idx == -1) {
+      return null;
+    }
+    const [result] = contacts.splice(idx, 1);
+    console.log(`Contact with id=${contactId} was removed`);
+    updateContacts(contacts);
+    return result;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
 
-//     // result code!!!!
-
-//     // console.log(result);
-
-//     return result;
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// }
-
-// getContactById(5);
-// function removeContact(contactId) {
-//   // ...твой код
-// }
-
-// async function addContact(name, email, phone) {
-//   try {
-//     // console.log(name, email, phone);
-//     await fs.appendFile(contactsPath, [
-//       {
-//         name: name.toString(),
-//         email: email.toString(),
-//         phone: phone.toString(),
-//       },
-//     ]);
-//     console.log("yes");
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// }
-// addContact("asdasd", "sadsad", "sdasdsa");
+async function addContact(name, email, phone) {
+  try {
+    const contacts = await listContacts();
+    const newContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+    };
+    updateContacts(contacts);
+    console.log(`Contact with name="${name}" was added to database.`);
+    return newContact;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
